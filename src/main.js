630905247c375b6f84b779768923346428ad91cc
Christian Hamburger Grøngaard <christianhg@gmail.com>
2017-03-13 13:28:02 +0100

    🎉

---
+++ src/main.js
@@ -0,0 +1,7 @@
+import 'reveal.js/css/reveal.css'
+import 'reveal.js/css/theme/solarized.css'
+import './main.scss'
+
+import Reveal from 'reveal.js'
+
+Reveal.initialize({})
---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 9000
+  },
+  devtool: 'cheap-module-source-map',
+  entry: './src/main.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'webpack-introduction.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(css|scss)$/,
+        use: ExtractTextPlugin.extract({
+          use: [
+            {
+              loader: 'css-loader'
+            },
+            {
+              loader: 'sass-loader'
+            }
+          ]
+        })
+      },
+      {
+        test: /\.(eot|ttf|woff)$/,
+        use: {
+          loader: 'file-loader'
+        }
+      }
+    ]
+  },
+  node: {
+    fs: 'empty'
+  },
+  plugins: [
+    new ExtractTextPlugin('webpack-introduction.css'),
+    new HtmlWebpackPlugin({
+      title: 'Webpack Introduction',
+      template: 'src/index.ejs'
+    })
+  ],
+  resolve: {
+    extensions: ['.js', '.scss']
+  }
+};

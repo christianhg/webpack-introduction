767de1b121e4db5ef922454e0ecfb79125e32a5c
Christian Hamburger Grøngaard <christianhg@gmail.com>
2017-03-16 09:13:05 +0100

    Add highlight.js support and Solarized theme

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import 'reveal.js/css/reveal.css'
+import 'reveal.js/css/theme/solarized.css'
 import './main.scss'
 import Reveal from 'reveal.js'
 import markdown from './markdown'
@@ -6,3 +7,5 @@ import markdown from './markdown'
 Reveal.initialize()
 
 markdown.initialize(Reveal)
+
+import './highlight.js'
